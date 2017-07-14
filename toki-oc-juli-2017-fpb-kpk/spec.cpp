3b2ec778a4a64c9daed03935a5c1687064a0631d
Wiwit Rifa'i <wiwitrifai@gmail.com>
2017-07-15 00:51:33 +0700

    Add more test cases

--- toki-oc-juli-2017-fpb-kpk/spec.cpp
+++ toki-oc-juli-2017-fpb-kpk/spec.cpp
@@ -75,18 +75,42 @@ protected:
       "3 2"
     });
   }
+
+  void SampleTestCase2() {
+    Input({
+      "3",
+      "1 4",
+      "2 2 5",
+      "3 1 3 6"
+    });
+    Output({
+      "1",
+      "2 1"
+    });
+  }
+
   void BeforeTestCase() {
       K.clear();
       X.clear();
   }
 
   void TestCases() {
-    CASE(N = 1, randomSize(1, int(1e5)), randomNumber(1, int(1e7)));
-    CASE(N = 10, randomSize(1, int(1e5)), randomNumber(1, int(1e7)));
-    CASE(N = 100, randomSize(1, int(1e5)), randomNumber(1, int(1e7)));
-    CASE(N = 1000, randomSize(1, int(1e5)), randomNumber(1, int(1e7)));
-    CASE(N = 10000, randomSize(1, int(1e5)), randomNumber(1, int(1e7)));
-    CASE(N = 100000, randomSize(1, int(1e5)), randomNumber(1, int(1e7)));
+    CASE(N = 2, randomSize(2, int(10)), randomNumber(1, int(1e3), 2 * 2 * 3));
+    CASE(N = 2, randomSize(2, int(100)), randomNumber(1, int(1e4), 97 * 101));
+    CASE(N = 10, randomSize(10, int(1e3)), randomNumber(1, int(1e5), 1));
+    CASE(N = 100, randomSize(100, int(1e4)), randomNumber(1, int(1e6), rnd.nextInt(1, int(1e6))));
+    CASE(N = 1, randomSize(1, int(1e5)), randomNumber(1, int(1e7), 1));
+    CASE(N = 1, randomSize(1, int(1e5)), consecutiveNumber(13));
+    CASE(N = 10, randomSize(100, int(1e5)), randomNumber(1, int(1e7), 32));
+    CASE(N = 10, randomSize(1000, int(1e5)), consecutiveNumber(rnd.nextInt(1, int(1e3))));
+    CASE(N = 100, randomSize(100, int(1e5)), randomNumber(1, int(1e7), rnd.nextInt(1, 1e7)));
+    CASE(N = 1000, randomSize(1, int(1e5)), randomNumber(1, int(1e7), 1));
+    CASE(N = 1000, randomSize(5, int(1e5)), randomNumber(1, int(1e7), 37 * 37));
+    CASE(N = 1000, randomSize(25, int(1e5)), randomNumber(1, int(1e7), 512 * 121));
+    CASE(N = 1000, randomSize(100, int(1e5)), randomNumber(1, int(1e7), rnd.nextInt(2, 1e7)));
+    CASE(N = 10000, randomSize(1, int(1e5)), randomNumber(1, int(1e7), 4099));
+    CASE(N = 20000, randomSize(1, int(1e5)), randomNumber(1, int(1e7), 1));
+    CASE(N = 100000, randomSize(1, int(1e5)), randomNumber(1, int(1e7), 2));
   }
 
 private:
@@ -103,12 +127,23 @@ private:
     }
   }
 
-  void randomNumber(int lo, int hi) {
+  void randomNumber(int lo, int hi, int minAns) {
     X.resize(N);
     for (int i = 0; i < N; i++) {
       X[i].resize(K[i]);
       for (int j = 0; j < K[i]; j++)
         X[i][j] = rnd.nextInt(lo, hi);
+      int z = rnd.nextInt(0, K[i]-1);
+      X[i][z] = minAns * rnd.nextInt((lo + minAns - 1)/ minAns, hi/minAns);
+    }
+  }
+
+  void consecutiveNumber(int start) {
+    X.resize(N);
+    for (int i = 0; i < N; i++) {
+      X[i].resize(K[i]);
+      for (int j = 0; j < K[i]; j++)
+        X[i][j] = start + j;
     }
   }
 };
