f0921e1abd395d915ec9a792fa06502272d49d8b
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2017-07-16 17:59:12 +0700

    Added tricky case in seven segment solution

--- toki-oc-juli-2017-seven-segment/solution.cpp
+++ toki-oc-juli-2017-seven-segment/solution.cpp
@@ -79,14 +79,22 @@ Segment SEG[TOTAL_SEGMENT] = {Segment(1, 1, 1, 1, 1, 1, 0),
 			 	 Segment(1, 1, 1, 1, 1, 1, 1),
 			 	 Segment(1, 1, 1, 1, 0, 1, 1)
 			 	};
+			 	
+Segment EmptySegment = Segment();
 
 int n;
 bool maks_from_awal = true;
+
+char num[N + 5];
 int max_num[N + 5];
+
 char correct[N + 5];
-int dp[N][2];
+int dp[N][2][2]; // dp[pos][still_max][still_zero]
 int val[N + 5];
 Segment ar[N + 5];
+
+int start_num;
+int start_n;
 
 void setup(){
 	char _arr[N + 5];
@@ -94,9 +102,15 @@ void setup(){
 	scanf("%d", &n);
 	scanf("%s", correct);
 	scanf("%s", _arr);
+	strcpy(num, _arr);
 
 	int lenArr = strlen(_arr);
-	if(lenArr > n) maks_from_awal = false;
+	if(lenArr > n) {
+		maks_from_awal = false;
+		for(int i = 0;i < n; ++i){
+			max_num[i] = _arr[i] - '0';
+		}
+	}
 	else{
 		int len_zero_digit = n - lenArr;
 		for(int i = 0;i < len_zero_digit; ++i){
@@ -117,35 +131,60 @@ void setup(){
 	memset(val, -1, sizeof val);
 }
 
-int rek(int pos, bool still_max){
+int rek(int pos, bool still_max, bool already_start){
 	if(pos == n) return 1;
-	if(dp[pos][still_max] == -1){
-		int &ret = dp[pos][still_max] = 0;
-		if(still_max){
-			int max_num_pos = max_num[pos];
-			if(val[pos] != -1){
-				if(val[pos] > max_num_pos) ret = 0;
-				else {
-					ret = rek(pos + 1, val[pos] == max_num_pos);
+	if(dp[pos][still_max][already_start] == -1){
+		int &ret = dp[pos][still_max][already_start] = 0;
+		if(!already_start){
+			int max_num_pos = still_max ? max_num[pos] : TOTAL_SEGMENT - 1;
+			if(val[pos] == -2){
+				if(pos == n - 1) ret = 0;
+				else ret = rek(pos + 1, false, false);
+			}
+			else if(val[pos] != -1){
+				if(val[pos] > max_num_pos || (val[pos] == 0 && pos != n-1)) ret = 0;
+				else{
+					ret = rek(pos + 1, still_max && (val[pos] == max_num_pos), true);
 				}
 			}
 			else{
-				for(int i = 0;i <= max_num_pos; ++i){
+				// special case where 0 exist!
+				if(pos == n - 1){
+					if(ar[pos].compatible(SEG[0])){
+						ret = rek(pos + 1, still_max && (0 == max_num_pos), true);
+					}
+				}
+				
+				// it still can be empty segment!!!
+				else if(ar[pos].compatible(EmptySegment)){
+					ret = rek(pos + 1, false, false);
+				}
+				
+				for(int i = 1;i <= max_num_pos; ++i){
 					if(ar[pos].compatible(SEG[i])){
-						ret += rek(pos + 1, i == max_num_pos);
+						ret += rek(pos + 1, still_max && (i == max_num_pos), true);
 						if(ret >= MOD) ret -= MOD;
 					}
-				}	
+				}
 			}
 		}
 		else{
-			if(val[pos] != -1){
-				ret = rek(pos + 1, false);
+			
+			if(val[pos] == -2){
+				return 0;
 			}
+			
+			int max_num_pos = still_max ? max_num[pos] : TOTAL_SEGMENT - 1;
+			if(val[pos] != -1){
+				if(val[pos] > max_num_pos) ret = 0;
+				else{
+					ret = rek(pos + 1, still_max && (val[pos] == max_num_pos), already_start);
+				}
+			} 
 			else{
-				for(int i = 0;i < TOTAL_SEGMENT; ++i){
+				for(int i = 0;i <= max_num_pos; ++i){
 					if(ar[pos].compatible(SEG[i])){
-						ret += rek(pos + 1, false);
+						ret += rek(pos + 1, still_max && (i == max_num_pos), already_start);
 						if(ret >= MOD) ret -= MOD;
 					}
 				}	
@@ -153,11 +192,18 @@ int rek(int pos, bool still_max){
 		}
 	}
 
-	return dp[pos][still_max];
+	return dp[pos][still_max][already_start];
 }
 
 void precheck(){
-	for(int i = 0;i < n; ++i){
+	
+	int lenNum = strlen(num);
+	int lenN = n;
+	
+	start_n = max(0, lenN - lenNum);
+	start_num = max(0, lenNum - lenN);
+	
+	for(int i = start_n; i < n; ++i){
 		if(correct[i] - '0'){
 			bool ketemu = false;
 			for(int j = 0;j < TOTAL_SEGMENT && !ketemu; ++j){
@@ -166,17 +212,34 @@ void precheck(){
 					val[i] = j;
 				}
 			}
+			
+			if(ar[i] == EmptySegment){
+				ketemu = true;
+				val[i] = -2;
+			}
+			
 			if (!ketemu){
 				puts("0");
 				exit(0);
 			}
 		}
 	}
+	
+	/* checking for pre-correct string*/
+	for(int i = 0; i < start_n; ++i){
+		if(ar[i] == EmptySegment) continue;
+		puts("0");
+		exit(0);
+	}
+	
+	if(start_num > 0){
+		maks_from_awal = false;
+	}
 }
 
 int main(){
 	setup();
 	precheck();
-	printf("%d\n", rek(0, maks_from_awal));
+	printf("%d\n", rek(start_n, maks_from_awal, false));
 	return 0;
 }
