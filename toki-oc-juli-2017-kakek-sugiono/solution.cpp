16eafb7e862003fa972c8d11e52ce6f92241f652
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2017-07-23 15:54:22 +0700

    soal berubah

--- toki-oc-juli-2017-kakek-sugiono/solution.cpp
+++ toki-oc-juli-2017-kakek-sugiono/solution.cpp
@@ -4,18 +4,33 @@ using namespace std;
 typedef long long ll;
 
 ll mod = 1e9 + 7;
+ll maxn = 1000;
+ll comb[maxn+10][maxn+10];
 
-ll pangkat(ll n) {
-  if (n == 0) return 1;
-  else if (n % 2) return (pangkat(n-1) * 2) % mod;
-  else {
-    ll tmp = pangkat(n / 2);
-    return (tmp*tmp) % mod;
+void computeCombination() {
+  memset(comb, 0, sizeof comb);
+  comb[0][0] = 1;
+
+  for(int i = 1; i <= maxn; i++) {
+    for(int j = 0; j <= i; j++) {
+      comb[i][j] = comb[i-1][j];
+      if (j > 0) comb[i][j] += comb[i-1][j-1];
+      comb[i][j] %= mod;
+    }
   }
 }
 
 int main() {
+  computeCombination();
+
   ll n;
   cin >> n;
-  cout << pangkat(n-1) << endl;
+
+  ll ans = 0;
+  for(int i = 0; i <= n; i+= 2) {
+    ans += (comb[n][i] * comb[i][i/2]) % mod;
+    ans %= mod;
+  }
+
+  cout << ans << endl;
 }
