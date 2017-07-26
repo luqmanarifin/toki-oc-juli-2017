97a44bce95006adf51becb74ed20545927fab20c
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2017-07-26 20:33:09 +0700

    Revise alt-solution for kakek sugiono

--- toki-oc-juli-2017-kakek-sugiono/alt-solution.cpp
+++ toki-oc-juli-2017-kakek-sugiono/alt-solution.cpp
@@ -1,7 +1,10 @@
 #include <cstdio>
 
 typedef long long LL;
-const int N = 1e9, MOD = 1e9 + 7;
+const int N = 1e3, MOD = 1e9 + 7;
+
+LL fact[N + 5];
+LL modpowfact[N + 5];
 
 LL sqr(LL x, int mod = MOD){
 	return (x * x) % MOD;
@@ -14,16 +17,26 @@ LL modpow(int x, int y, int mod){
 	if (y & 1) tmp = (tmp * x) % mod;
 	return tmp;
 }
+inline void precompute(){
+	fact[0] = 1;
+	fact[1] = 1;
+	modpowfact[0] = modpowfact[1] = 1;
+	for(int i = 2;i <= N; ++i){
+		fact[i] = fact[i - 1] * i;
+		fact[i] %= MOD;
+		modpowfact[i] = modpow(fact[i], MOD - 2, MOD);
+	}
+}
 
 int main(){
-
+	precompute();
 	int n;
 	scanf("%d", &n);
-	if(n == 0){
-		puts("1");
+	LL total = 0;
+	for(int i = 0;i <= n; i += 2){
+		total += (((fact[n] * modpowfact[n - i]) % MOD) * sqr(modpowfact[i/2])) % MOD;
+		total %= MOD;
 	}
-	else{
-		printf("%lld\n", modpow(2, n-1, MOD));
-	}
+	printf("%lld\n", total);
 	return 0;
 }
